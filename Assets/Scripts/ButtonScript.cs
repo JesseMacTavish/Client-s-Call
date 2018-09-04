45f1da76224778600d519cf3c81ae08363a8808e
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-09-04 14:45:54 +0200

    Upgraded dialog box

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.Select();
+    }
+
+}
