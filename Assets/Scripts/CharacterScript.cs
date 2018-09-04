86cce33792e76ee8a9fb05f32bd1033ef0a336ba
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-09-04 16:33:28 +0200

    Kill da santa

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    public GameObject dialog;
+   
+    void OnTriggerEnter(Collider other)
+    {
+        dialog.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        dialog.SetActive(false);
+    }
+
+    public void KillSanta()
+    {
+        Destroy(gameObject);
+        Destroy(dialog);
+    }
+
+}
