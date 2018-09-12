a5078a2ce6c5f64d9b0c381f498461eec5260999
Henrik Strømme <henstr3@gmail.com>
2018-09-12 11:50:08 +0200

    Changed some animation stuff

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
         {
             //Die
             //GetComponent<PlayerAnimation>().DeathAnimation();
+            Destroy(gameObject);
         }
     }
 
